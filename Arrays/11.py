5f444bd744694f7428609e9cf866ceb91087f21e
Austin Snyder <aumaro.nyc@gmail.com>
2020-03-25 15:57:29 -0700

    updates

--- Arrays/11.py
+++ Arrays/11.py
@@ -1,18 +1,13 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        if len(height) == 0:
-            return 0
-        maximum = 0
-        l = 0
-        r = len(height) - 1
-        while l != r:
-            amt = 0
-            small = height[l] if height[l] <= height[r] else height[r]
-            amt = small * (r - l)
-            if amt > maximum:
-                maximum = amt
-            if height[l] <= height[r]:
-                l += 1
+        left = 0
+        right = len(height) - 1
+        max_water = 0
+        while left < right:
+            volume = min(height[left],height[right]) * (right - left)
+            max_water = volume if volume > max_water else max_water
+            if height[left] <= height[right]:
+                left += 1
             else:
-                r -= 1
-        return maximum
+                right -= 1
+        return max_water
