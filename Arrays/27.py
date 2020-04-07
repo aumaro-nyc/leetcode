3ff6f52b1f20a90f7f72349e0445fa45e81f56aa
Austin <aumaro.nyc@gmail.com>
2020-04-07 08:55:55 -0600

    27

---
+++ Arrays/27.py
@@ -0,0 +1,12 @@
+class Solution:
+    """
+    Time: 59% (32ms)
+    """
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                nums.remove(nums[i])
+            else:
+                i += 1
+        return len(nums)
